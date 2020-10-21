812ab094f0f14abca6257c19732dcf21d3a6c59f
Rasmus Høegh <rasmusmalikhoegh@gmail.com>
2020-10-21 18:53:56 +0200

    [Finishes #175377549] Setup simple test workflow draft.

---
+++ src/test_pipeline.py
@@ -0,0 +1,16 @@
+import pytest
+from pipeline import BasePipeline
+
+def test_basepipeline():
+    modules = {
+        'datasets': None,
+        'preprocessings': None,
+        'models': None,
+        'postprocessings': None,
+        'metrics': None,
+        'reports': None,
+    }
+    pipeline = BasePipeline(**modules)
+    
+    with pytest.raises(NotImplemented):
+        pipeline.run()
